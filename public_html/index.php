e511dbc81ca474df4fd37662d5403fb2691ecdb4
Nathan Sanchez <nsanchez121@cnm.edu>
2016-10-11 13:41:30 -0600

    woo we gettin it

---
+++ public_html/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>html sandboox</title>
+	</head>
+	<body>
+
+		<!--nathan you have no clue what your doing huh? -->
+		<header>
+			<h1>hello this is nathans page</h1>
+		</header>
+		<!-- sick dood-->
+		<main>
+
+			<!-- dont get lost -->
+			<section>
+				<p>blah blah blah blah blah blah vlakom ONDFNNWNNFN;AND;NNfnsndlkweoindfnlsknenps
+					nfpnenwblah blah blah blah blah blah vlakom ONDFNNWNNFN;AND;NNfnsndlkweoindfnlsknenpsnfpnenw</p>
+				<img src="img/download.jpeg" alt="meow go meow meow"
+			</section>
+			<section>
+				<h2></h2>
+				<ul></ul>
+			</section>
+			<section>
+
+			</section>
+		</main>
+	</body>
+</html>
